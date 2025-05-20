5d237b9f0e7ae07285186a302966f7866cc42ec7
mtempesta-eulotech <74406663+mtempesta-eulotech@users.noreply.github.com>
2025-05-20 13:41:58 +0200

    Add files via upload

--- src/tools/query.ts
+++ src/tools/query.ts
@@ -10,15 +10,18 @@ export class QueryTools extends BaseTools {
         server.tool(
             "query",
             "Query the database with a SQL statement",
-            { query: z.string().describe("The SQL query to execute") },
+            {
+				databaseID: z.string().describe("L'id del database da interrogare"),
+				query: z.string().describe("The SQL query to execute")
+			},
             tools.query.bind(tools)
         );
 
         return tools;
     }
 
-    async query({ query }: { query: string }) {
-        const results = await database.query(query);
+    async query({ databaseID, query }: { databaseID:string, query: string }) {
+        const results = await database.query(databaseID, query);
         if (!results.length) return this.toResult("No results found for the query.");
         return this.toResult(JSON.stringify(results));
     }
