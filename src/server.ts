3c66de54693aba197956109569b9fdcf123a9afa
mtempesta-eulotech <74406663+mtempesta-eulotech@users.noreply.github.com>
2025-05-20 17:01:26 +0200

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import { name, version } from "../package.json";
 import { SchemaTools } from "./tools/schema";
 import { QueryTools } from "./tools/query";
 import { TransactionTools } from "./tools/transactions";
-
+/*
 const { DB_CONF } = process.env;
 if (!DB_CONF) {
 	console.error("Dichiarare la variabile d'ambiente DB_CONF come un json avente chiave l'id del database e valore un json con (DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE) are required");
 	process.exit(1);
-}
+}*/
 
 const server = new McpServer({ name, version });
 
